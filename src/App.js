6f15cee8f10949f005f8b710431a28de262b18a4
oscar <u3572091@connect.hku.hk>
2023-01-28 02:01:05 +0800

    clear select value

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { Select } from "antd";
+import { useState } from "react";
+
+const indicatorOptions = {
+  macd: {a: 0, b: 0},
+  rsi: {a: 0, b: 0}
+}
+
+const selectOptions = Object.keys(indicatorOptions).map(value => ({
+  label: value, value: value
+}));
 
 function App() {
+  const [indicators, setIndicators] = useState([]);
+  const [value, setValue] = useState(null);
+
+  const handleAddIndicator = selectedIndicator => {
+    setIndicators(currentList => {
+      const values = [...currentList];
+      values.push({
+        indicator: selectedIndicator, ...indicatorOptions[selectedIndicator]
+      });
+      return values;
+    });
+    setValue(null);
+  };
+
+  const handleRemovePlayers = (index) => {
+    setIndicators(currentList => {
+      const values = [...currentList];
+      values.splice(index, 1);
+      return values;
+    })
+  };
+
+  const handleInputChange = (index, e) => {
+    setIndicators(currentList => {
+      const values = [...currentList];
+      const updatedField = e.target.name;
+      values[index][updatedField] = e.target.value;
+      return values;
+    })
+  };
+
+  const handleSubmit = () => {
+    console.log(indicators);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Select
+        onSelect={handleAddIndicator}
+        options={selectOptions}
+        value={value}
+      />
+      <>
+        {indicators.map((indicator, outerIndex) => (
+          <div style={{display: "flex"}} key={outerIndex}>
+            {Object.keys(indicator).map((field, innerIndex) => (
+              field == "indicator"? 
+                <p key={`${outerIndex}-${innerIndex}`}>{indicator[field]}</p>: 
+                <input name={field} onChange={e => handleInputChange(outerIndex, e)} placeholder={field} key={`${outerIndex}-${innerIndex}`} />
+            ))}
+            <button onClick={() => handleRemovePlayers(outerIndex)} key={outerIndex}>Remove</button>
+          </div>
+        ))}
+      </>
+      <button onClick={handleSubmit}>Submit</button>
+    </>
   );
 }
 
